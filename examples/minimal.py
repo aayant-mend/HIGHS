4bb3d129cac9ca2a218173611169f98d4933e7cf
Aayan Thalakola <aayan.thalakola@mend.io>
2024-03-19 15:44:42 -0500

    first commit

---
+++ examples/minimal.py
@@ -0,0 +1,14 @@
+# This example does NOT work yet.
+# We are working on it! 
+
+import highspy
+
+h = highspy.Highs()
+
+x1 = h.addVar(lb=-h.inf)
+x2 = h.addVar(lb=-h.inf)
+
+h.addConstr(x2 - x1 >= 2)
+h.addConstr(x1 + x2 >= 0)
+
+h.minimize(x2)
